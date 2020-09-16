aa02f8e78e78f2af8f82b93b53f2fda42f3e56bd
Jhovius <70752290+Jhovius@users.noreply.github.com>
2020-09-16 12:47:36 +0200

    Add files via upload

---
+++ src/h05/StaafAutomatisch.java
@@ -0,0 +1,32 @@
+package h05;
+
+import java.awt.*;
+import java.applet.*;
+
+
+
+public class  StaafAutomatisch extends Applet {
+    public void paint(Graphics g) {
+
+        g.setColor(Color.red);
+        g.fillRect(145,230,8,100);
+        g.drawRect(145,230,8,100);
+        g.setColor(Color.RED);
+        g.drawString("Valerie,40 KG",160,300);
+
+        g.setColor(Color.blue);
+        g.fillRect(115,170,8,140);
+        g.drawRect(165,260,8,140);
+        g.setColor(Color.blue);
+        g.drawString("Hans,80 KG",110,325);
+
+        g.setColor(Color.green);
+        g.fillRect(85,140,8,180);
+        g.drawRect(195,300,8,180);
+
+        g.drawString("Jeroen, 100 KG",30,335);
+
+    }
+
+}
+
