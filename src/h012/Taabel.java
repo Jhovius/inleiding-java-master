1b85d7e66da0af86136c6e21132f443248d7475f
johanhovius <jwhovius16@gmail.com>
2020-11-17 12:52:44 +0100

    Opdracht 6 van  12

--- src/h012/Taabel.java
+++ src/h012/Taabel.java
@@ -5,26 +5,27 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Index extends Applet {
+public class Taabel extends Applet {
     double getal[];
+    int timesFound;
     Button knop;
     TextField textvak;
     String result = "";
 
-    public void init(){
+    public void init() {
         getal = new double[8];
 
-        getal[0]=2;
-        getal[1]=3.4;
-        getal[2]=4.4;
-        getal[3]=4.8;
-        getal[4]=5.2;
-        getal[5]=7.4;
-        getal[6]=9;
-        getal[7]=11;
+        getal[0] = 2;
+        getal[1] = 3.4;
+        getal[2] = 4.4;
+        getal[3] = 4.8;
+        getal[4] = 5.2;
+        getal[5] = 7.4;
+        getal[6] = 9;
+        getal[7] = 11;
 
-        knop=new Button();
-        textvak=new TextField("",20);
+        knop = new Button();
+        textvak = new TextField("", 20);
         knop.setLabel("ok");
 
         add(knop);
@@ -35,23 +36,23 @@ public class Index extends Applet {
 
     }
 
-    public void paint(Graphics g){
-        g.drawString("Dit is :" + result, 50, 20);
+    public void paint(Graphics g) {
+        g.drawString("Dit is :" + timesFound, 50, 20);
     }
 
     class vakListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            String input  = textvak.getText();
+            String input = textvak.getText();
             double inputDouble = Double.parseDouble(input); //make it a double
-            for (int teller = 0; teller < getal.length; teller++){
-                if (getal[teller] == inputDouble){
+            for (int teller = 0; teller < getal.length; teller++) {
+                if (getal[teller] == inputDouble) {
                     result = "Match gevonden op index: " + teller + "!";
-                    break;
+                    timesFound++;
                 }
                 result = "Geen match!";
             }
             repaint();
-           // System.out.println(input);
+            // System.out.println(input);
         }
     }
 }
